6b0c2ac348d323e0251c211e8d4a21d1c8808f16
MyslivetsOleg <157577921+MyslivetsOleg@users.noreply.github.com>
2025-06-20 11:41:55 +0300

    Update main.tf

--- sectools-terraform/main.tf
+++ sectools-terraform/main.tf
@@ -12,7 +12,18 @@ terraform {
       version = ">= 1.4.1"
     }
   } 
- backend "s3" {} 
+ backend "s3" {
+    bucket                     = "sectools-cicd-bucket"
+    key                        = "tfstate/sectools.tfstate"
+    region                     = "us-east-1"
+    endpoint                   = "http://minio.sectools.lab:9000"
+    access_key                 = var.minio_access_key
+    secret_key                 = var.minio_secret_key
+    force_path_style           = true
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_requesting_account_id  = true
+  }
 }
 
 provider "opennebula" {
